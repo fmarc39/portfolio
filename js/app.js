922ad6b393d12108a95d49ce1a33c8a58ece8510
Marc François <fmarc@MacBook-Pro-de-Marc.local>
2021-03-31 13:25:33 +0200

    debut refacto js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,43 @@
 // CREATION DE LA PARTIE QUI VA AFFICHER L'OVERLAY SECONDAIRE AU MOMENT DU HOVER
 
+const init = () => {
+  console.log('ok')
+}
+
 const projectElts = document.getElementById("container-projects")
-const overlay1 = document.getElementById("overlay-1")
-const overlay2 = document.getElementById("overlay-2")
-const overlay3 = document.getElementById("overlay-3")
-const overlay11 = document.getElementById("overlay-1-1")
-const overlay22 = document.getElementById("overlay-2-2")
-const overlay33 = document.getElementById("overlay-3-3") 
+const overlay1 = document.getElementById("overlay-1-1")
+const overlay2 = document.getElementById("overlay-2-2")
+const overlay3 = document.getElementById("overlay-3-3") 
 
 projectElts.addEventListener("mouseover", function (event) {
   let targetElt = event.target
 
   if(targetElt.id == "overlay-1") {
-    overlay11.classList.remove('hiden') 
+    overlay1.classList.remove('hiden') 
   }
 
   if(targetElt.id == "overlay-2") {
-    overlay22.classList.remove('hiden')
+    overlay2.classList.remove('hiden')
   }
 
   if(targetElt.id == "overlay-3") {
-    overlay33.classList.remove('hiden')    
+    overlay3.classList.remove('hiden')    
   }
 })
 
 function mouseleave1() {
 
-  overlay11.classList.add('hiden') 
+  overlay1.classList.add('hiden') 
 }
 
 function mouseleave2() {
 
-  overlay22.classList.add('hiden')
+  overlay2.classList.add('hiden')
 }
 
 function mouseleave3() {
 
-  overlay33.classList.add('hiden')
+  overlay3.classList.add('hiden')
 }
 
 // CREATION DE LA SECTION QUI VA PERMETTRE D'OBERSVER LA SCROLL DE LA PAGE ET DE FAIRE APPARAITRE LES EFFETS 
@@ -48,7 +49,7 @@ const options = {
     threshold: ratio
   }
 
-var callback = function(entries, observer) { 
+let callback = function(entries, observer) { 
   entries.forEach(entry => {
 
       if (entry.intersectionRatio > ratio) {
@@ -56,11 +57,10 @@ var callback = function(entries, observer) {
       }
   }
 )
-
   }
-  var observer = new IntersectionObserver(callback, options);
+  let observer = new IntersectionObserver(callback, options);
 
-  var target = document.querySelectorAll("[class*=reveal-").forEach(function (r) {
+  let target = document.querySelectorAll("[class*=reveal-").forEach(function (r) {
     observer.observe(r)
   })
 
@@ -74,8 +74,8 @@ var callback = function(entries, observer) {
 
   function responsive() {
 
-    var header__navbar = document.querySelector('.header__navbar')
-    var navBar = document.getElementById("navbar");
+    let header__navbar = document.querySelector('.header__navbar')
+    let navBar = document.getElementById("navbar");
     if (navBar.className === "navbar__elts-box") {
       navBar.className += " responsive";
       header__navbar.className += " clicked"
@@ -86,10 +86,10 @@ var callback = function(entries, observer) {
   }
 // Animated main title letres 
 
-var message = "Développeur Full-Stack JS";
-var msgCount = 0;
-var timer1;
-var messageLabel = document.getElementById("messageLabel");
+let message = "Développeur Full-Stack JS";
+let msgCount = 0;
+let timer1;
+let messageLabel = document.getElementById("messageLabel");
 
 function textFunc() {
   messageLabel.innerHTML = message.substring(0, msgCount);
@@ -100,3 +100,5 @@ function textFunc() {
   }
 }
 timer1 = setInterval("textFunc()", 100);
+
+window.addEventListener("load", init);
